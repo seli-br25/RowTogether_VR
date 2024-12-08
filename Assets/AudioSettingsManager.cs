061d708655b4c764c01f568438511dfc749dc2f5
01604987 <e1604987@student.tuwien.ac.at>
2024-12-08 22:14:57 +0100

    implemented audio volume control & mute

--- Assets/AudioSettingsManager.cs
+++ Assets/AudioSettingsManager.cs
@@ -1,30 +1,88 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Voice.Unity;
+using TMPro;
+using Photon.Voice.Unity.UtilityScripts;
 
 public class AudioSettingsManager : MonoBehaviour
 {
 
     [SerializeField]
-    Button VolumeUp;
+    private Button muteButton;
     [SerializeField]
-    Button VolumeDown;
+    private MicAmplifier amplifier;
     [SerializeField]
-    Button Mute;
+    private TextMeshProUGUI volumeDisplay;
     [SerializeField]
-    private Recorder recorder;
+    private Sprite muteSprite;
+    [SerializeField]
+    private Sprite unMuteSprite;
+
+    private Image muteButtonImage;
+
+    private uint volume;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        volume = 5;
+        volumeDisplay.text = volume.ToString();
+        amplifier.AmplificationFactor = volume * 2;
+        muteButtonImage = muteButton.GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    public void OnClickVolDown()
+    {
+        if (volume > 1)
+        {
+            volume -= 1;
+            DisplayVolume();
+            SetGain();
+        }
+    }
+
+    public void OnClickVolUp()
+    {
+        if (volume < 10)
+        {
+            volume += 1;
+            DisplayVolume();
+            SetGain();
+        }
+    }
+
+    private void DisplayVolume()
+    {
+        volumeDisplay.text = volume.ToString();
+    }
+
+    private void SetGain()
+    {
+        amplifier.AmplificationFactor = volume * 2;
+    }
+
+    public void OnClickMute()
+    {
+        // Mute
+        if (amplifier.AmplificationFactor > 0)
+        {
+            amplifier.AmplificationFactor = 0;
+
+            muteButtonImage.sprite = muteSprite;
+        } 
+        // UnMute
+        else
+        {
+            amplifier.AmplificationFactor = volume * 2;
+
+            muteButtonImage.sprite = unMuteSprite;
+        }
     }
 }
